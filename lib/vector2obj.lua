2c077d128ef93903816dbf11cdf581e2d869f34c
ZachCurtis <31259055+ZachCurtis@users.noreply.github.com>
2017-12-04 08:41:53 -0500

    fixed issue with returning always negative y

--- lib/vector2obj.lua
+++ lib/vector2obj.lua
@@ -1,9 +1,9 @@
-[[
+--[[
 Dec 2017
 Zach Curtis AKA InfinityDesign
 Simple Vector2obj with metadata for use in linear regression
 vector2obj.data, vector2obj.answer, vector2obj.bias
-]]
+]]--
 
 local vector2obj = {}
 
@@ -13,7 +13,7 @@ function vector2obj.new(maxX,maxY)
 	local v2new = {}
 	setmetatable(v2new, vector2obj)
 	
-	v2new.data = {math.random() * math.random(-maxX, maxX),math.random() * math.random(-maxY, -maxY), 1} --index 3 is a bias towards constant 1 or true using step method
+	v2new.data = {math.random() * math.random(-maxX, maxX),math.random() * math.random(-maxY, maxY), 1} --index 3 is a bias towards constant 1 or true using step method
 	if v2new.data[1] > v2new.data[2] then
 		v2new.answer = 1
 	else
