cc94e7ab5090e08afeac53f6b93fd43a528c9fcf
ZachCurtis <31259055+ZachCurtis@users.noreply.github.com>
2017-12-04 07:23:28 -0500

    added bias constant

--- lib/vector2obj.lua
+++ lib/vector2obj.lua
@@ -1,7 +1,10 @@
 [[
-simple vector2 object with an answer calculated using linear regression
-used for training simple perceptron
+Dec 2017
+Zach Curtis AKA InfinityDesign
+Simple Vector2obj with metadata for use in linear regression
+vector2obj.data, vector2obj.answer, vector2obj.bias
 ]]
+
 local vector2obj = {}
 
 vector2obj._index = vector2obj
@@ -9,6 +12,8 @@ vector2obj._index = vector2obj
 function vector2obj.new(maxX,maxY)
 	local v2new = {}
 	setmetatable(v2new, vector2obj)
+	v2new.bias = 1 --constant bias 
+	
 	v2new.data = {math.random() * math.random(-maxX, maxX),math.random() * math.random(-maxY, -maxY)}
 	if v2new.data[1] > v2new.data[2] then
 		v2new.answer = 1
