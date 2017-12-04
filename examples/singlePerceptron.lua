86e41ec9363e8922f9530d7ce8fdd721b828ce7b
ZachCurtis <31259055+ZachCurtis@users.noreply.github.com>
2017-12-04 07:32:00 -0500

    fixed comments

--- examples/singlePerceptron.lua
+++ examples/singlePerceptron.lua
@@ -1,4 +1,4 @@
-[[
+--[[
 roblox script object on server
 
 
@@ -9,9 +9,13 @@ secondly runs a new perceptron with a larger control set reusing same training d
 the amount of errors before and after training
 
 requires /src/nerualNetworks/perceptron.lua and /lib/vector2obj.lua as children module scripts
-]]
+]]--
+
+
 local perceptron = require(script.perceptron) --stop calling methods on this
 local v2obj = require(script.vector2obj)
+
+
 
 function generateTestData(inputNum, amt, point) --point is Vector2 array of max sizes
 	if inputNum == 2 then --vector2obj for ease of acess
@@ -30,7 +34,7 @@ print('single perceptron neural network testing')
 print('we are training with 50 vector2 points in range (+/-100, +/-75)')
 local testData = generateTestData(2, 50, {100,75})
 
-local tester = perceptron.new(2) --call methods on this constructed object
+local tester = perceptron.new(3) --call methods on this constructed object
 --inital weights are generated randomly in the constructor
 
 print('the intial choice with our control data, (75,-23), is')
@@ -51,7 +55,7 @@ print('--')print('-')print('--')
 print('that went well but lets mesure training a new perceptron with the same training set')
 print('but this time we will calculate an error precentange off a large dataset before training')
 
-local newPercept = perceptron.new(2, .1)
+local newPercept = perceptron.new(3, .1)
 
 local controlData = generateTestData(2,25,{100,75})
 local errSum = 0
@@ -61,7 +65,7 @@ for i = 1, #controlData do
 		errSum = errSum + 1
 	end
 end
-print(errSum .. ' out of our ' .. #controlData .. ' control points were incorrect')
+print(errSum .. ' out of our ' .. #controlData .. 'control points were incorrect')
 --print('that gives us an error precentage of ' .. #controlData/errSum)
 
 print('training with original training set')
@@ -81,5 +85,7 @@ for i = 1, #controlData do
 end
 print(errSum .. ' out of our ' .. #controlData .. ' control points were incorrect')
 --print('that gives us an error precentage of ' .. errSum/#controlData)
+
+print('--')print('-')print('--')
 
 
