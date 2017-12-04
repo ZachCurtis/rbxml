f51b2865157136fcee928bb7363ac6f41322bca4
ZachCurtis <31259055+ZachCurtis@users.noreply.github.com>
2017-12-04 14:44:36 -0500

    added methodology for perceptron

--- lib/vector2obj.lua
+++ lib/vector2obj.lua
@@ -7,20 +7,35 @@ vector2obj.data, vector2obj.answer, vector2obj.bias
 
 local vector2obj = {}
 
-vector2obj._index = vector2obj
+vector2obj.__index = vector2obj
 
 function vector2obj.new(maxX,maxY)
 	local v2new = {}
 	setmetatable(v2new, vector2obj)
-	
+	v2new.m = _G.slope
+	v2new.b = _G.yInter
 	v2new.data = {math.random() * math.random(-maxX, maxX),math.random() * math.random(-maxY, maxY), 1} --index 3 is a bias towards constant 1 or true using step method
-	if v2new.data[1] > v2new.data[2] then
-		v2new.answer = 1
-	else
-		v2new.answer = -1
-	end
+	wait()	
+	v2new:calculateAnswer(v2new.data[1])
 	
 	return v2new
+end
+
+function vector2obj:getDataForPerceptron()
+	local range = (200 - -200)
+	local newRange = 2
+	local x1 = (((self.data[1] - -200) * 2) / range) +-1
+	local y1 = (((self.data[2] - -200) * 2) / range) +-1
+	return {x1, y1, 1}
+end
+
+function vector2obj:calculateAnswer(x)
+	local yLine = self.m * x + self.b
+	if self.data[2] > yLine then
+		self.answer = 1
+	else
+		self.answer = -1
+	end
 end
 
 return vector2obj
